f9be0ddf574b47b5d3bcbed194cb287a6f4e4acc
Asit <asit199@gmail.com>
2021-06-05 00:05:03 +0530

    Order Application Command and Query classes setup.

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
+    {
+
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UpdateOrderCommandHandler> _logger;
+
+        public UpdateOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<UpdateOrderCommandHandler> logger)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToUpdate = await _orderRepository.GetByIdAsync(request.Id);
+            if(orderToUpdate==null)
+            {
+                _logger.LogError("Order not exist in the database");
+            }
+
+            // in the below line, request is the source and orderToUpdate is the destination object
+            //object Map(object source, object destination, Type sourceType, Type destinationType);
+            _mapper.Map(request, orderToUpdate, typeof(UpdateOrderCommand), typeof(Order));
+
+            await _orderRepository.UpdateAsync(orderToUpdate);
+
+            _logger.LogInformation($"Order {orderToUpdate.Id} is successfully updated.");
+
+            return Unit.Value;
+        }
+    }
+}
